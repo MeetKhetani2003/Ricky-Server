afc74a78af7613f175b50b7bb803d5fd297ac418
MeetKhetani2003 <meetkhetani1111@gmail.com>
2025-03-08 16:14:15 +0530

    nodemon added

---
+++ src/utils/mailer.js
@@ -0,0 +1,18 @@
+import nodemailer from 'nodemailer';
+
+import { MAIL_ID, MAIL_PASSWORD } from './serverConfig.js';
+
+export default nodemailer.createTransport({
+  service: 'Gmail',
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: MAIL_ID,
+    pass: MAIL_PASSWORD,
+  },
+});
+
+//for get app password
+//https://myaccount.google.com/apppasswords?
+azxi vago nsnz bvwj
